91171d8bee1ae2b9ae8b0d83391bf3b5c33138d6
marcus-mcmullenjr <marcus.mcmullenjr@gmail.com>
2024-11-28 10:55:39 -0500

    Initial commit

---
+++ src/Dummy.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> skillList = new ArrayList<>();
+		skillList.add("Trainee");
+		skillList.add("Beginner");
+		skillList.add("Novice");
+		skillList.add("Intermediate");
+		skillList.add("Expert");
+		skillList.add("Specialist");
+		skillList.add("Beast");
+		
+		List<String> wSplitList = new ArrayList<>();
+		wSplitList.add("Trainee");
+		wSplitList.add("Beginner");
+		wSplitList.add("Novice");
+		wSplitList.add("Intermediate");
+		wSplitList.add("Expert");
+		wSplitList.add("Specialist");
+		wSplitList.add("Beast");
+
+	}
+
+}
